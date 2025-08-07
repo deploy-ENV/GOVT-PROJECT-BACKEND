ddf748f98e0d59ae9cd73d4b3fed1d3b5e8c1fa4
deploy-ENV <foxomaniac@gmail.com>
2025-08-07 13:56:02 +0530

    Update CorsConfig.java

--- src/main/java/org/govt/CorsConfig.java
+++ src/main/java/org/govt/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://your-react-frontend.onrender.com")
+                        .allowedOrigins("http://localhost:3000", "https://gov-ashen-iota.vercel.app/")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
